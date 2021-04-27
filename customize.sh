ed7d8a7ba2cd5849ed09f1e6942dbb755b1b57b3
charles <chevalier.charles@gmail.com>
2021-04-27 13:26:42 +0200

    Fixed service and added @reboot support to service

--- customize.sh
+++ customize.sh
@@ -6,3 +6,5 @@ CRONTABS_PATH=/data/crontabs
 
 mkdir -p $CRONTABS_PATH
 touch $CRONTABS_PATH/root
+
+set_perm "$MODPATH/service.sh"  0  0  0755
--- service.sh
+++ service.sh
@@ -4,3 +4,10 @@ MODDIR=${0%/*}
 CRONTABS_PATH=/data/crontabs
 
 crond -c $CRONTABS_PATH
+
+# @reboot is not implemented in busybox crond, we do it manually
+cat /data/crontabs/root | grep @reboot | while read -r line ; do
+  command="${line:8}"
+  echo "Starting command $command"
+  eval $command &
+done
